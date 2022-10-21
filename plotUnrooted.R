3fff1ff261643da2c5251910fb56be7beeb3af3c
joshrest <63354322+joshrest@users.noreply.github.com>
2022-10-21 15:27:45 -0400

    Add files via upload

--- plotUnrooted.R
+++ plotUnrooted.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 library(ape)
 print(paste("now printing an unrooted tree from",args[1],"for user:`",Sys.getenv("MYGIT"),"`... Output is in ", args[2]))
 tree <- unroot(read.tree(args[1]))
-
+fsize <- 0.4
+if(!(is.na((as.numeric(args[3])>0)))){fsize <- as.numeric(args[3])}
 pdf(args[2])
-plot.phylo(tree,type="unrooted",cex=as.numeric(args[3]))
+plot.phylo(tree,type="unrooted",cex=fsize)
 dev.off()
-
